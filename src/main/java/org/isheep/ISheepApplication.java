e3d414ca5e3e64cbb4adfc67e9cb23495fdabbf3
anthonyraymond <raymond.anthony.63@hotmail.fr>
2016-11-07 16:09:42 +0100

    init

---
+++ src/main/java/org/isheep/ISheepApplication.java
@@ -0,0 +1,12 @@
+package org.isheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISheepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISheepApplication.class, args);
+	}
+}
